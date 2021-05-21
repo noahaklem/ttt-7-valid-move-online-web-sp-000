9a08b4702afcee37b1442d2a0d1c951adc93d473
industrious-agent-153d <klem.noah@gmail.com>
2021-05-21 00:53:03 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  index.between?(1, 9)
+  index.between?(1, 9) = FALSE
   invalid = FALSE
   if position_taken?(board, index)
     invalid = FALSE
