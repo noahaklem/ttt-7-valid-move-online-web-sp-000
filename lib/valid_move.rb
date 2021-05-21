719b449722ce4bd30c05b38e24ed5b20095bff9c
industrious-agent-153d <klem.noah@gmail.com>
2021-05-21 00:51:01 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,20 @@
 # code your #valid_move? method here
+def valid_move?(board, index)
+  index.between?(1, 9)
+  invalid = FALSE
+  if position_taken?(board, index)
+    invalid = TRUE
+  end
+end
 
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if (board[index] == " X " || board[index] == "X")
+    TRUE
+    
+    elsif (board[index] == " O " || board[index] == "O")
+    TRUE
+    
+  else (board[index] == " " || board[index] == "" || board[index] == nil)
+    FALSE
+  end
+end
